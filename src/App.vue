e72db5dc8abeb776d37071f6325db2687b851459
Sofia <sofiahliv51@gmail.com>
2022-09-22 16:07:23 +0300

    1

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <template>
-<div>
+<div id="app">
   <div class="screen">
-    <input v-model="operand1" />
-    {{ operator}}
-    <input v-model="operand2" />
+    <input v-model.number="operand1" />
+    {{ item }}
+    <input v-model.number="operand2" />
     =
-    {{ result}}
+    {{ result }}
+
   </div>
   <div class="keyboard">
-    <button @click = "operator = '+'">+</button>
-    <button @click = "operator = '-'">-</button>
-    <button @click = "operator = '*'">*</button>
-    <button @click = "operator = '/'">/</button>
-    <button @click = "operator = '^'">^</button>
-    <button @click = "operator = '%'">%</button>
-    <button @click = "getResult">=</button>
+    <button v-for="(item, index) in operators"
+            v-bind:key="index"
+        @click="getResult(item)">{{ item }}</button>
+<!--    <button @click = "operator = '-'">-</button>-->
+<!--    <button @click = "operator = '*'">*</button>-->
+<!--    <button @click = "operator = '/'">/</button>-->
+<!--    <button @click = "operator = '^'">^</button>-->
+<!--    <button @click = "operator = '%'">%</button>-->
+<!--    <button @click = "getResult">=</button>-->
+  </div>
+  <input type="checkbox" id="checkbox" v-model="checked">
+  <label for="checkbox">Открыть экранную клавиатуру</label>
+  <div  v-show="checked">
+    <button v-for="(num, index) in numbers"
+            v-bind:key="index"
+            @click="input(num)">{{ num }}
+    </button>
+    <button @click="reset">R</button>
+    <div>
+    <input type="radio" id="one" value="1"  v-model="picked">
+    <label for="one">Один</label>
+    <br>
+    <input type="radio" id="two" value="2" v-model="picked">
+    <label for="two">Два</label>
+    </div>
   </div>
 </div>
 </template>
@@ -26,15 +45,20 @@ export default {
   name: 'App',
   data() {
     return {
-      operand1: 0,
-      operand2: 0,
-      operator: 0,
-      result: 0
+      operand1: " ",
+      operand2: " ",
+      result: 0,
+      checked: false ,
+      operators: ['+', '-', '*', '/', '^', '%'],
+      numbers: [0,1,2,3,4,5,6,7,8,9],
+      test: " ",
+      picked: "1",
     }
   },
   methods: {
-    getResult() {
-      switch (this.operator) {
+
+    getResult(operator) {
+      switch (operator) {
         case '+':
           this.result = Number(this.operand1) + Number(this.operand2)
               break;
@@ -58,12 +82,33 @@ export default {
             this.result = "Error"
               break;
       }
+    },
+    input: function(char) {
+      if (this.picked === "1") {
+        this.operand1 = this.operand1.toString();
+        this.operand1+=char;
+      } else {
+        this.operand2 = this.operand2.toString();
+        this.operand2+=char;
+      }
+
+    },
+    reset() {
+      this.operand1 = " ";
+      this.operand2 = " ";
+      this.result = 0;
     }
+
   }
+
 
 }
 </script>
 
-<style>
+<style lang="css">
+ #app {
 
+  text-align: center;
+  margin-top: 60px;
+}
 </style>
